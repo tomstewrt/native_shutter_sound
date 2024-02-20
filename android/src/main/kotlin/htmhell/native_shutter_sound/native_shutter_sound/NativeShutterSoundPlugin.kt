945ab5bc23f0041aaf1847a4a9d1b7b1e10b289c
tomstewrt <thomasastewart99@gmail.com>
2024-02-20 15:47:34 +0000

    set volume to system level before playing sound for android

--- android/src/main/kotlin/htmhell/native_shutter_sound/native_shutter_sound/NativeShutterSoundPlugin.kt
+++ android/src/main/kotlin/htmhell/native_shutter_sound/native_shutter_sound/NativeShutterSoundPlugin.kt
@@ -16,6 +16,7 @@ class NativeShutterSoundPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "native_shutter_sound")
@@ -24,8 +25,14 @@ class NativeShutterSoundPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "play") {
+      // Get the current volume level for the media stream
+      val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+      // Set the volume level for the media stream to the same value and show the volume UI
+      audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0)
       val sound = MediaActionSound()
       sound.play(MediaActionSound.SHUTTER_CLICK)
+      // Optionally, restore the volume level for the media stream to the original value
+      // audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, originalVolume, 0)
     } else {
       result.notImplemented()
     }
